4ec0998943383fb151266cc33ff7c6417642bb19
ch27a <141591933+ch27a@users.noreply.github.com>
2025-01-13 18:27:06 +0100

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,37 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let newNum = 0;
+    let lowNum;
+    let highNum;
+
+    if (!(typeof(firstNum) === "number") || !(typeof(secondNum) === "number"))
+    {
+        return "ERROR"
+    }
+    if (firstNum < 0 || secondNum < 0)
+    {
+        return "ERROR"
+    }
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum))
+    {
+        return "ERROR"
+    }
+
+    if (firstNum <= secondNum)
+    {
+        lowNum = firstNum;
+        highNum = secondNum;
+    }
+    else
+    {
+        lowNum = secondNum;
+        highNum = firstNum;
+    }
+
+    for (let i = lowNum; i <= highNum; i++)
+    {
+        newNum += i;
+    }
+    return newNum;
 
 };
 
