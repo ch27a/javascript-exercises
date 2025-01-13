36de7be1efc7d248671b5247e8fca03682be6706
ch27a <141591933+ch27a@users.noreply.github.com>
2025-01-13 18:13:50 +0100

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    for (const arg of theArgs)
+    {
+        for (let i = 0; i < array.length; i++)
+        {
+            if (array[i] === arg)
+            {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
