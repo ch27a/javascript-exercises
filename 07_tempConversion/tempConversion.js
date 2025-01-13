cb4525ec10225083bfc19fc23399caa83811f311
ch27a <141591933+ch27a@users.noreply.github.com>
2025-01-13 19:01:39 +0100

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+  temperatureCelsius = (temperatureFahrenheit - 32) * (5 / 9);
+  return Number(temperatureCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCelsius) {
+  temperatureFahrenheit = (temperatureCelsius * (9 / 5)) + 32;
+  return Number(temperatureFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
